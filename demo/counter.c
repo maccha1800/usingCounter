00bfef61ba69ffc36f0add3fa63e3e8517ccfcee
maccha1800 <masato.baseball.1008@gmail.com>
2024-04-02 23:03:41 +0900

    不快度計算追加

--- demo/counter.c
+++ demo/counter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
@@ -8,20 +9,46 @@
 } typedef Man;
 */
 
-// カウンターを示す配列
-int counter[5] = {0, 0, 0, 0, 0};
+int *counter; // カウンター
+int SeetNum;  // 席数
+int *LWall;   // 壁(左)
+int *RWall;   // 壁(右)
+
+/* カウンターの初期設定(入力は席数)  */
+void initializeCounter(int n)
+{
+    SeetNum = n;
+    LWall = (int *)malloc(sizeof(int) * (SeetNum + 2));
+    counter = LWall + 1;
+    RWall = LWall + SeetNum + 1;
+
+    *LWall = 0;
+    *RWall = 0;
+
+    for (int i = 0; i < SeetNum; i++)
+    {
+        counter[i] = 0;
+    }
+
+    printf("addres-------\r\nLWall   : %p\r\ncounter : %p\r\nRWall   : %p\r\n-------------\r\n\r\n", LWall, counter, RWall);
+}
+
+void finalizeCounter()
+{
+    free(LWall);
+}
 
 /*カウンターの状況を画面に出力する関数*/
 void stateCounter(char *str)
 {
     char time[10];
 
-    // 人席目は仕切りを入れない
-    sprintf(str, "%d", counter[0]);
-    for (int i = 1; i < 5; i++)
+    // 1席目は仕切りを入れない
+    sprintf(str, "%3d", counter[0]);
+    for (int i = 1; i < SeetNum; i++)
     {
-        sprintf(time, "%d", counter[i]);
-        strcat(str, "|");
+        sprintf(time, "%3d", counter[i]);
+        strcat(str, " | ");
         strcat(str, time);
     }
 }
@@ -33,9 +60,9 @@ void stateCounter(char *str)
 int addMan()
 {
     // 滞在時間算出（最大値8）
-    int a = rand() % 8 + 1;
+    int a = rand() % 16 + 1;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < SeetNum; i++)
     {
         if (counter[i] == 0)
         {
@@ -48,22 +75,45 @@ int addMan()
 }
 
 // 不快度の総量を計算する関数
-// TODO
-// int calcUncomfortable()
-// {
-//     int score;
-//     for (int i = 0; i < 4; i++)
-//     {
-//         if (counter[i] != 0 && counter[i] != 0)
-//         {
-//             score++;
-//         }
-//     }
-// }
+int calcUncomfortable()
+{
+    int score = 0;
+    int left;
+    int right;
+
+    // 人の隣に人がいる場合、一人につき不快度を1数える
+    for (int i = 0; i < SeetNum; i++)
+    {
+        int *seet = counter + i;
+
+        if (*seet == 0)
+        {
+            continue;
+        }
+
+        left = *(seet - 1);
+        right = *(seet + 1);
+
+        if (left != 0)
+            score++;
+        if (right != 0)
+            score++;
+    }
+    return score;
+}
 
 int main(void)
 {
     srand((unsigned int)time(NULL));
+
+    printf("start\r\n");
+
+    char str[100];
+
+    initializeCounter(5);
+    stateCounter(str);
+    printf("s : %s\r\n", str);
+
     int count = 0;
 
     for (count = 0; count < 100; count++)
@@ -73,11 +123,10 @@ int main(void)
             addMan();
         }
 
-        char str[100];
         stateCounter(str);
-        printf("%000d : %s\r\n", count, str);
+        printf("%03d : %s  (%d)\r\n", count, str, calcUncomfortable());
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < SeetNum; i++)
         {
             if (counter[i] != 0)
             {
@@ -86,5 +135,8 @@ int main(void)
         }
     }
 
+    finalizeCounter();
+
+    printf("finish\r\n");
     return 0;
 }
